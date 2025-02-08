8460dc079ce360d482997fb6373891fab2fb1c68
llllyyyynnnn <157394687+llllyyyynnnn@users.noreply.github.com>
2025-02-08 16:37:08 +0100

    project files

---
+++ Utilities/ProcessUserTime.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+namespace ADTEfficiencyMeasurements.Utilities;
+
+public class ProcessUserTime
+{
+    public double ElapsedTotalSeconds { get; private set; }
+
+    public void Restart()
+    {
+        processUserTimeStart = Process.GetCurrentProcess().UserProcessorTime.TotalSeconds;
+    }
+
+    public void Stop()
+    {
+        ElapsedTotalSeconds = Process.GetCurrentProcess().UserProcessorTime.TotalSeconds - processUserTimeStart;
+    }
+
+    private double processUserTimeStart;
+}
